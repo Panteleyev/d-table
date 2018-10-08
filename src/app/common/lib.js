9b038f9924106b41f8d5bc5c2c227df60073eb2d
ototaiyo <otohoshi@gmail.com>
2018-10-09 02:41:21 +0300

    Заменил module.exports на exports

--- src/app/common/lib.js
+++ src/app/common/lib.js
@@ -1,12 +1,11 @@
 /**
  * JS библиотека
  */
-module.exports = {
-  /**
-   * Генератор случайного числового значения между min (включительно) и max (не включая max)
-   * @param {number} min - минимальное значение
-   * @param {number} max - максимальное значение
-   * @returns {*}
-   */
-  getRandomInt: (min, max) => Math.floor(Math.random() * (max - min)) + min,
-};
+
+/**
+ * Генератор случайного числового значения между min (включительно) и max (не включая max)
+ * @param {number} min - минимальное значение
+ * @param {number} max - максимальное значение
+ * @returns {number}
+ */
+export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
